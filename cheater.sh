c6c62cbfb8f457e0ef6426bf79f7326027dd1424
Linux-RISC <rbpiuser@gmail.com>
2022-11-04 19:48:18 +0100

    Add files via upload

--- cheater.sh
+++ cheater.sh
@@ -26,6 +26,7 @@ fi
 
 }
 
+sleep 10
 device="/dev/ttyUSB0"
 stty -F $device 9600 -parenb -parodd -cmspar cs8 -hupcl -cstopb cread clocal -crtscts -ignbrk -brkint -ignpar -parmrk -inpck -istrip -inlcr -igncr -icrnl -ixon -ixoff -iuclc -ixany -imaxbel -iutf8 -opost -olcuc -ocrnl onlcr -onocr -onlret -ofill -ofdel nl0 cr0 tab0 bs0 vt0 ff0 -isig -icanon -iexten -echo echoe echok -echonl -noflsh -xcase -tostop -echoprt echoctl echoke -flusho -extproc
 
